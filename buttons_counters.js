e3f4a3fee66254d938b5ac0d0b448d57d4541ec0
redaktorscha <39005857+redaktorscha@users.noreply.github.com>
2018-07-24 13:17:33 +0300

    js new version buttons className

--- buttons_counters.js
+++ buttons_counters.js
@@ -1,33 +1,18 @@
+// Увеличивает счетчик конкретной кнопки, обрезает последние 3 символа текста кнопки
+// @param btn Object Кнопка, на которую пользователь нажал
 
-const counters = {
-    b1: 0,
-    b2: 0,
-    b3: 0,
-    b4: 0,
-    b5: 0,
-    b6: 0,
-    b7: 0,
-    b8: 0,
-    b9: 0
-  };
-  
-  let buttons = {};
-  let keys = Object.keys(counters);
-  
-  for (let i = 0; i < keys.length; i++)
-  {
-    let elems = document.getElementById(keys[i]);
-    elems.onclick = count;  
-    buttons[keys[i]] = elems.innerHTML.slice(0, -3);
-   
-  }
-  
-  
-  function count(btn)
-  {
-    let id = btn.target.id;
-    counters[id]++;
-    btn.target.innerHTML = buttons[id] + " (" + counters[id] + ")";  
-  }
-  
-  
+const count = (btn) => {
+  let elem = btn.target;
+  let id = elem.id;
+  counters[id]++;
+  elem.innerHTML = elem.innerHTML.slice(0, -3) + " (" + counters[id] + ")";
+}
+
+let counters = {};
+
+let buttons = document.getElementsByClassName("b_cl");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = count;
+  counters[buttons[i].id] = 0;
+}
