b0d40ea7ec5a45b0694af8c8500a32503fa227d8
redaktorscha <39005857+redaktorscha@users.noreply.github.com>
2018-07-25 03:14:18 +0300

    buttons-counters fixed

--- buttons_counters.js
+++ buttons_counters.js
@@ -5,14 +5,15 @@ const count = (btn) => {
   let elem = btn.target;
   let id = elem.id;
   counters[id]++;
-  elem.innerHTML = elem.innerHTML.slice(0, -3) + " (" + counters[id] + ")";
+  btn.target.innerHTML = buttonNames[id] + " (" + counters[id] + ")";
 }
 
 let counters = {};
-
+let buttonNames = {};
 let buttons = document.getElementsByClassName("b_cl");
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].onclick = count;
   counters[buttons[i].id] = 0;
+  buttonNames[buttons[i].id] = buttons[i].innerHTML.slice(0, -3);
 }
